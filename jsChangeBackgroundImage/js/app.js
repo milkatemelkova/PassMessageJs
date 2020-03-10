6a43e1e404fddd19bb184ef6d757e73685a1a7a6
milkatemelkova <50911887+milkatemelkova@users.noreply.github.com>
2020-03-10 16:21:10 +0200

    Add files via upload

---
+++ jsChangeBackgroundImage/js/app.js
@@ -0,0 +1,16 @@
+// immediate invoked function expression
+
+(function() {
+  const pictures = [
+    "contBcg-0",
+    "contBcg-1",
+    "contBcg-2",
+    "contBcg-3",
+    "contBcg-4"
+  ];
+
+let counter = 0;
+
+const btn = document.querySelectorAll('.btn');
+
+})();
